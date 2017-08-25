2067affe5eebccaa01d659264faa8caa7f2ac476
Admin <danny.garcia80@hotmail.com>
2017-08-25 14:20:54 -0700

    edit js file and css

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ $(document).ready(function() {
     var cheese = $('input[name=cheese]:checked').val();
     var vegetables = $('input:checkbox[name=vegetables]:checked').map(function() {
         return this.value;
+      }).get();
+    var meats = $('input:checkbox[name=meats]:checked').map(function() {
+        return this.value;
+      }).get();
+
+// Back End Business Logic
+function Pizza(size, crust, cheese, vegetables, meats) {
+  this.size = size;
+  this.crust = crust;
+  this.cheese = cheese;
+  this.vegetables = vegetables;
+  this.meats = meats;
+  this.price;
+}
